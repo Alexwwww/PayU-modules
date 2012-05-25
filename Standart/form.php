669aac77cfb1e214f9ef0a1b37bb52e5a1dd9d6c
root <root@it-a-820m.devel.entp>
2012-05-25 13:56:01 +0300

    Update a standart module

--- Standart/form.php
+++ Standart/form.php
@@ -2,13 +2,12 @@
 header("Content-type:text/html; charset=utf-8");
 
 include_once "PayU.cls.php";
+$option  = array( 'merchant' => 'MERCHANT', 'secretkey' => 'SECRETKEY', 'debug' => 1 );
+
 
 # Create form for request
-if( !isset($_GET['answer']) )
-{
-	$forSend = array (
+$forSend = array (
 					#'ORDER_REF' => $orderID, # Uniqe order 
-					#'ORDER_DATE' => date("Y-m-d H:i:s"), # Date of paying ( Y-m-d H:i:s ) 
 					'ORDER_PNAME' => array( "Test_goods", "Тест товар №1", "Test_goods3" ), # Array with data of goods
 					'ORDER_PCODE' => array( "testgoods1", "testgoods2", "testgoods3" ), # Array with codes of goods
 					'ORDER_PINFO' => array( "", "", "" ), # Array with additional data of goods
@@ -18,38 +17,37 @@ if( !isset($_GET['answer']) )
 					'ORDER_SHIPPING' => 0, # Shipping cost
 					'PRICES_CURRENCY' => "UAH",  # Currency
 					'LANGUAGE' => "RU",
-					'BILL_FNAME' => "TEST"
+					'BILL_FNAME' => "TEST" # ...  etc.
 				  );
 
-# For debug mode off remove second argument at pushArray method
-$pay = PayU::getInst()->setMerch( "MERCHANT", 'SECRET_KEY')->pushArray( $forSend, 1 ); 
-
-
-#------------
-#	Variants for showing form
-#	1. echo $pay
-#	2. echo $pay->form
-#	
-#	Show form at another place (when class PayU initialized)
-#	1.	$pay2 = PayU::getInst();
-#		echo $pay2;
-#	2. 	echo PayU::getInst();
-#
-#-----------
-echo $pay; 
-
+if( !isset($_GET['answer']) )
+{
+	$pay = PayU::getInst()->setOptions( $option )->setData( $forSend )->LU();
+	echo $pay;
 }
-
-
 
 # Read answer (IPN)
 if( isset($_GET['answer']) )
 {
-	$payansewer = PayU::getInst()->setMerch('MERCHANT', 'SECRET_KEY')->checkSign();
+	$payansewer = PayU::getInst()->setOptions( $option )->IPN();
 	echo $payansewer;
 }
 
 
 
 
+
+
+
+
+
 ?>
+<!---
+<form>
+Write (LU)
+Read (IPN)
+Request (IDN)
+Refund (IRN)
+
+</form>
+--->
