c81d078d9486c4e943ce4d3494bf21e875f22cd4
Dmitriy <root@PAYU-P-3.devel.entp>
2012-07-07 14:49:24 +0300

    Add Magento 1.7 module

--- Bitrix/payu/payment.php
+++ Bitrix/payu/payment.php
@@ -1,7 +1,8 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 include  dirname(__FILE__)."/payu.cls.php"; 
 
-$ORDER_ID = $arResult['ORDER_ID'];
+if ( isset($arResult['ORDER_ID']) ) $ORDER_ID = $arResult['ORDER_ID'];
+  else $ORDER_ID = (int)$_GET['ORDER_ID'];
 
 #------------------------------------------------
 # Recive all items data
@@ -10,29 +11,17 @@ $ORDER_ID = $arResult['ORDER_ID'];
 $arBasketItems = array();
 
 $dbBasketItems = CSaleBasket::GetList(
-          array(
-                "NAME" => "ASC",
-                "ID" => "ASC"
-              ),
-          array(
-                "LID" => SITE_ID,
-                "ORDER_ID" => $ORDER_ID
-              ),
-          false,
-          false,
-          array("ID", "CALLBACK_FUNC", "MODULE", 
-                "PRODUCT_ID", "QUANTITY", "DELAY", 
-                "CAN_BUY", "PRICE", "WEIGHT")
+          array( "NAME" => "ASC", "ID" => "ASC" ),
+          array( "LID" => SITE_ID, "ORDER_ID" => $ORDER_ID ),
+          false, false,
+          array("ID", "CALLBACK_FUNC", "MODULE", "PRODUCT_ID", "QUANTITY", "DELAY", "CAN_BUY", "PRICE", "WEIGHT")
     );
+
 while ($arItems = $dbBasketItems->Fetch())
 {
     if (strlen($arItems["CALLBACK_FUNC"]) > 0)
     {
-        CSaleBasket::UpdatePrice($arItems["ID"], 
-                                 $arItems["CALLBACK_FUNC"], 
-                                 $arItems["MODULE"], 
-                                 $arItems["PRODUCT_ID"], 
-                                 $arItems["QUANTITY"]);
+        CSaleBasket::UpdatePrice($arItems["ID"], $arItems["CALLBACK_FUNC"], $arItems["MODULE"], $arItems["PRODUCT_ID"], $arItems["QUANTITY"]);
         $arItems = CSaleBasket::GetByID($arItems["ID"]);
     }
     $arBasketItems[] = $arItems;
@@ -46,15 +35,29 @@ $db_res = CSaleOrderPropsValue::GetList(($b=""), ($o=""), array("ORDER_ID"=>$ORD
 while ($ar_res = $db_res->Fetch())
    $arCurOrderProps[(strlen($ar_res["CODE"])>0) ? $ar_res["CODE"] : $ar_res["ID"]] = $ar_res["VALUE"];
 
+
+$option  = array( 'merchant' => 'MERCHANT', 'secretkey' => 'SECRETKEY', 'debug' => 1 );
+
+
 $PayU = new PayU( CSalePaySystemAction::GetParamValue("MERCHANT"), CSalePaySystemAction::GetParamValue("SECURE_KEY") );
 $PayU->url = CSalePaySystemAction::GetParamValue("LU_URL");
 
-$orderID = "TESTPAYU_".$ORDER_ID."_".CSaleBasket::GetBasketUserID()."_". md5( "payuOrder_".time() );
+$orderID = "PayuOrder_".$ORDER_ID."_".CSaleBasket::GetBasketUserID()."_". md5( "payuOrder_".time() );
 
-  $forSend = array (
+ $backref = CSalePaySystemAction::GetParamValue("BACK_REF");
+  
+
+ $forSend = array (
           'ORDER_REF' => $orderID, # Uniqe order 
-          'ORDER_DATE' => date("Y-m-d H:i:s"), # Date of paying ( Y-m-d H:i:s ) 
+          #'ORDER_DATE' => date("Y-m-d H:i:s"), # Date of paying ( Y-m-d H:i:s ) 
+          'ORDER_SHIPPING' => $arOrder['PRICE_DELIVERY'],
+          'PRICES_CURRENCY' => CSalePaySystemAction::GetParamValue("PRICE_CURRENCY"), # Currency
+          'DISCOUNT' => $arOrder['DISCOUNT_VALUE'],
+          'LANGUAGE' => CSalePaySystemAction::GetParamValue("LANGUAGE"),
+
           );
+
+if ( $backref != "" ) $forSend['BACK_REF'] = $backref;
 
 
 foreach ( $arBasketItems as $val )
@@ -67,7 +70,7 @@ foreach ( $arBasketItems as $val )
   $forSend['ORDER_VAT'][] = $val['VAT_RATE'];
 }
  
-  
+ /* 
   $forSend += array(
                     'ORDER_SHIPPING' => $arOrder['PRICE_DELIVERY'], # Shipping cost
                     'PRICES_CURRENCY' => CSalePaySystemAction::GetParamValue("PRICE_CURRENCY"), # Currency
@@ -79,7 +82,7 @@ foreach ( $arBasketItems as $val )
 
   $backref = CSalePaySystemAction::GetParamValue("BACK_REF");
   if ( $backref != "" ) $PayU->data['BACK_REF'] = $backref;
-
+*/
 
   $form = $PayU->getForm();
 
